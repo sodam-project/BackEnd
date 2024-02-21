6c316f9e05c30247fe1337fff00293a0d463b562
13155a1 <ppy040@gmail.com>
2024-02-21 19:53:51 +0900

    [FEAT] Create visit Controller and Service

---
+++ server/src/main/java/ssodamproject/server/visit/visitService.java
@@ -0,0 +1,9 @@
+package ssodamproject.server.visit;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class visitService {
+}
