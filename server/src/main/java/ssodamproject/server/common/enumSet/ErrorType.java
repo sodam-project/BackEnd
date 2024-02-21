d7bba55d8c51de7cbe6f367ca1758030a7065a52
13155a1 <ppy040@gmail.com>
2024-02-21 19:48:22 +0900

    [FEAT] Setting api and exception

---
+++ server/src/main/java/ssodamproject/server/common/enumSet/ErrorType.java
@@ -0,0 +1,16 @@
+package ssodamproject.server.common.enumSet;
+
+import lombok.Getter;
+
+@Getter
+public class ErrorType {
+
+    private int code;
+    private String message;
+
+    //  Error Type 생성자 생성
+    ErrorType(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
