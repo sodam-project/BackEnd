3330e87064041b668e2a6556aaffb8efa8ad7265
sunghyun1356 <sunghyun1356@naver.com>
2024-02-25 13:16:46 +0900

    [CHOR] Handler Delete

--- server/src/main/java/ssodamproject/server/GPT/WebsocketConfig.java
+++ server/src/main/java/ssodamproject/server/GPT/WebsocketConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import ssodamproject.server.GPT.Service.StreamCompletionHandler;
 
 @Configuration
 @EnableWebSocket
